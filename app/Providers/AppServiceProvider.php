054b6d54422264a75ce4b75e95b53bb13d8dacf2
Abraham Romero <romero9804@hotmail.com>
2025-07-08 21:34:51 -0700

    feat: crear funcionalidad para enviar de correo para confirmar cuenta

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function($notifiable,$url){
+            return (new MailMessage)->subject('Verificar cuenta')
+            ->line('Tu cuenta esta casi lista, solo es necesario que verifiques tu cuenta para que puedas empezar a usarla')
+            ->action('Confirmar cuenta', $url)
+            ->line('Si no creaste esta cuenta puedes ignorar este correo');
+        });
     }
 }
