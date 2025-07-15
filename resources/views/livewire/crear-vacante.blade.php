f10195342c50206397be288dcd4dac87b6e3e05c
Abraham Romero <romero9804@hotmail.com>
2025-07-14 21:44:22 -0700

    feat: crear componente de formulario de vacante

---
+++ resources/views/livewire/crear-vacante.blade.php
@@ -0,0 +1,40 @@
+<form class="md:w-1/2 space-y-5">
+    <div>
+        <x-input-label for="titulo" :value="__('Titulo vacante')" />
+        <x-text-input id="titulo" class="block mt-1 w-full" type="text" name="titulo" :value="old('titulo')" placeholder="Titulo vacante"/>
+        <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
+    </div>
+    <div>
+        <x-input-label for="sueldo" :value="__('Sueldo mensual')" />
+        <select id="sueldo" class="block mt-1 w-full" name="titulo"/>
+        </select>
+        <x-input-error :messages="$errors->get('sueldo')" class="mt-2" />
+    </div>
+    <div>
+        <x-input-label for="categoria" :value="__('Categoria')" />
+        <select id="categoria" class="block mt-1 w-full" name="categoria"/>
+        </select>
+        <x-input-error :messages="$errors->get('categoria')" class="mt-2" />
+    </div>
+    <div>
+        <x-input-label for="empresa" :value="__('Empresa')" />
+        <x-text-input id="empresa" class="block mt-1 w-full" type="text" name="empresa" :value="old('empresa')" placeholder="Empresa: ej. Netflix, Uber, Shopify"/>
+        <x-input-error :messages="$errors->get('empresa')" class="mt-2" />
+    </div>
+    <div>
+        <x-input-label for="ultimo_dia" :value="__('Ultimo dia para postularse')" />
+        <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" name="ultimo_dia" :value="old('ultimo_dia')"/>
+        <x-input-error :messages="$errors->get('ultimo_dia')" class="mt-2" />
+    </div>
+    <div>
+        <x-input-label for="descripcion" :value="__('Descriptcion del puesto')" />
+        <textarea id="descripcion" class="block mt-1 w-full h-72" name="ultimo_dia">
+        </textarea>
+    </div>
+    <div>
+        <x-input-label for="imagen" :value="__('Imagen')" />
+        <input type="file" id="imagen" class="block mt-1 w-full" name="imagen">
+    </div>
+
+    <x-primary-button>Crear vacante</x-primary-button>
+</form>
